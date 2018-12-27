c4e55acf5a8125999d2c11c3ca6f7500bf5224b7
c10t <c10t@fa.ke>
2018-12-28 02:42:11 +0900

    add methods

--- golang/vec3.go
+++ golang/vec3.go
@@ -6,10 +6,24 @@ type Vec3 struct {
 	X, Y, Z float64
 }
 
+var UnitVector = Vec3{X: 1, Y: 1, Z: 1}
+
 func (v Vec3) Length() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
 }
 
 func (v Vec3) SquaredLength() float64 {
 	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
+}
+
+func Add(v Vec3, w Vec3) Vec3 {
+	return Vec3{v.X + w.X, v.Y + w.Y, v.Z + w.Z}
+}
+
+func Sub(v Vec3, w Vec3) Vec3 {
+	return Vec3{v.X - w.X, v.Y - w.Y, v.Z - w.Z}
+}
+
+func (v Vec3) Scale(t float64) Vec3 {
+	return Vec3{v.X * t, v.Y * t, v.Z * t}
 }
--- golang/vec3_test.go
+++ golang/vec3_test.go
@@ -18,3 +18,27 @@ func TestSquaredLength(t *testing.T) {
 		t.Fatalf("SquaredLength should be return 50 for the vector (3, 4, 5)")
 	}
 }
+
+func TestAdd(t *testing.T) {
+	v := Vec3{X: 1.5, Y: -0.2, Z: -1}
+	w := Add(v, UnitVector)
+	if w.X != 2.5 || w.Y != 0.8 || w.Z != 0 {
+		t.Fatalf("(1.5, -0.2, -1) + (1, 1, 1) != (2.5, 0.8, 0)")
+	}
+}
+
+func TestSub(t *testing.T) {
+	v := Vec3{X: 0.5, Y: 1, Z: 1.7}
+	w := Sub(v, UnitVector)
+	if w.X != -0.5 || w.Y != 0 || w.Z != 0.7 {
+		t.Fatalf("(0.5, 1, 1.7) - (1, 1, 1) != (-0.5, 0, 0.7)")
+	}
+}
+
+func TestScale(t *testing.T) {
+	v := Vec3{X: 1, Y: -1, Z: 0}
+	w := v.Scale(-1.5)
+	if w.X != -1.5 || w.Y != 1.5 || w.Z != 0 {
+		t.Fatalf("-1.5 * (1, -1, 0) != (-1.5, 1.5, 0): (%v, %v, %v)", w.X, w.Y, w.Z)
+	}
+}
