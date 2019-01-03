957ee0352be6501b66988508d81416cc3693f246
c10t <c10t@fa.ke>
2019-01-04 03:50:01 +0900

    positionable camera

--- golang/camera.go
+++ golang/camera.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"math"
+)
+
 type Camera struct {
 	LowerLeftCorner Vec3
 	Horizontal      Vec3
@@ -13,6 +17,18 @@ func NewCamera() *Camera {
 	v := Vec3{0, 2, 0}
 	o := Vec3{0, 0, 0}
 	return &Camera{l, h, v, o}
+}
+
+func NewVerticalCamera(vfov, aspect float64) *Camera {
+	theta := vfov * math.Pi / 180
+	halfHeight := math.Tan(theta / 2)
+	halfWidth := aspect * halfHeight
+	l := Vec3{X: -halfWidth, Y: -halfHeight, Z: -1.0}
+	h := Vec3{X: 2 * halfWidth, Y: 0, Z: 0}
+	v := Vec3{X: 0, Y: 2 * halfHeight, Z: 0}
+	o := Vec3{X: 0, Y: 0, Z: 0}
+
+	return &Camera{LowerLeftCorner: l, Horizontal: h, Vertical: v, Origin: o}
 }
 
 func (c *Camera) GetRay(u, v float64) Ray {
